0d44f459904d40e6bf85fca3a14ae9ed4d0c26cf
R-Reichenbach <rafareichenbach@gmail.com>
2023-06-20 07:55:29 -0300

    modify

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+    include('cria_sessao.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prova_2</title>
+</head>
+<body>
+    <h3>Página Inicial - Prova PRW</h3>
+    <hr>
+    <ul>
+        <li><a href="cadastro_usuario.html">Cadastro fluxo de caixa</a></li>
+        <li><a href="listar_usuarios.php">Listagem de fluxo de caixa</a></li>
+        <li><a href="listar_usuarios.php">Consulta saldo caixa</a></li>
+    </ul>
+</body>
+</html>
